9d2e92cdf495c7eeffcddc72336f9adf52211409
RubiaMirianne <rubiamirianne@gmail.com>
2020-09-29 12:11:27 -0300

    commit

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -35,11 +35,6 @@
                 </ul>
             </div>
             </nav>
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
