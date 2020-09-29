572f6906597bda572c5689fbad308d198e6b989e
RubiaMirianne <rubiamirianne@gmail.com>
2020-09-29 15:47:45 -0300

    commit

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -89,7 +89,7 @@
                                     <form method="POST" action="{{ route('register') }}">
                                         @csrf
                                         <div class="form-group row">
-                                            <label for="vendedor">{{ __('Vendedor') }}</label>
+                                            <label for="vendedor" class="col-md-4 col-form-label text-md-right">{{ __('Vendedor') }}</label>
 
                                             <div class="col-md-6">
                                                 <input type="radio" id="vendedor" name="vendedor" value="vendedor">
