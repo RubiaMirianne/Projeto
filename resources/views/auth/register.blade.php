1a505b8a2952319b2b7ce336b9608f298631eb33
RubiaMirianne <rubiamirianne@gmail.com>
2020-09-29 12:29:13 -0300

    commit

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -41,7 +41,8 @@
             <div class="card">
                 <div class="card-header">{{ __('Register') }}</div>
 
-                <div class="card-body">
+                <div class="card cd col-12">
+                    <div class="card-body title"> Cadastro</div>
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
 
