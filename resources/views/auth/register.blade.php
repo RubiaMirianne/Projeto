800963d0f1d4ec933531a11c94cd14d15bb17619
RubiaMirianne <rubiamirianne@gmail.com>
2020-09-29 15:10:08 -0300

    commit

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -78,7 +78,7 @@
     </nav>>
                 <div class="container">
                     <div class="row justify-content-center">
-                        <div class="card cd col-12">
+
                         <div class="col-md-8">
                             <div class="card">
                                 <div class="card-header">{{ __('Register') }}</div>
@@ -140,7 +140,7 @@
                                         <div class="form-group row mb-0">
                                             <div class="col-md-6 offset-md-4">
                                                 <button type="submit" class="btn btn-primary">
-                                                    {{ __('Register') }}
+                                                    {{ __('Cadastrar') }}
                                                 </button>
                                             </div>
                                         </div>
