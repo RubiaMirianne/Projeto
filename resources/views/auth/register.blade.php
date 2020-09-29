61d8c6629c5b61822f765822718fb7406dc5dbdf
RubiaMirianne <rubiamirianne@gmail.com>
2020-09-29 15:01:13 -0300

    commit

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -78,6 +78,7 @@
     </nav>>
                 <div class="container">
                     <div class="row justify-content-center">
+                        <div class="card cd col-12">
                         <div class="col-md-8">
                             <div class="card">
                                 <div class="card-header">{{ __('Register') }}</div>
