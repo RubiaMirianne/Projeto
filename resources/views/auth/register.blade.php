481aec3f4b735b326b2c6b59eb4e1a6422a3ed70
RubiaMirianne <rubiamirianne@gmail.com>
2020-09-29 15:14:35 -0300

    commit

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -80,7 +80,6 @@
                     <div class="row justify-content-center">
 
                         <div class="col-md-8">
-                            <div class="card">
                                 <div class="card cd col-12">
                                 <div class="card-header">{{ __('Cadastro') }}</div>
 
