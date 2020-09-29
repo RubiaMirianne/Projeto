451ae0fb226d8e4767aca7775d16f81d771e2988
RubiaMirianne <rubiamirianne@gmail.com>
2020-09-29 08:32:57 -0300

    commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -29,8 +29,8 @@
                     @else
                         <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Login</a>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
+                        @if (Route::has('cadastro'))
+                            <a href="{{ route('cadastro') }}" class="ml-4 text-sm text-gray-700 underline">Cadastro</a>
                         @endif
                     @endif
                 </div>
