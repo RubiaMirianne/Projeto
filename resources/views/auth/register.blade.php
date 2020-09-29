7f04dd8bbf5c1684889e7e0892aa6a8d920de2e7
RubiaMirianne <rubiamirianne@gmail.com>
2020-09-29 16:47:22 -0300

    commit

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -95,7 +95,7 @@
                                         </div>
 
                                         <div class="form-group row">
-                                            <label for="estoquista" class="col-md-4 col-form-label text-md-right">Estoquista</label>
+                                            <label for="estoquista" class="col-md-200 col-form-label text-md-right">Estoquista</label>
                                             <input class= "form-control" type="radio" id="estoquista" name="cadastro" value="estoquista">
                                         </div>
 
