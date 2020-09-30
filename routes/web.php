5d466252a22addd4f2d1a593c273fb8e4df9b855
RubiaMirianne <rubiamirianne@gmail.com>
2020-09-30 09:29:32 -0300

    commit

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/Lista Compras', [App\Http\Controllers\HomeController::class, 'index'])->name('Lista Compras');
+Route::get('/Inicio Estoquista', [App\Http\Controllers\HomeController::class, 'index'])->name('Inicio Estoquista');
